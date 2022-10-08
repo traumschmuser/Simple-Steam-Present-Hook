738533d5caac12f3253f401027164c175d90cb40
traumschmuser <93420770+traumschmuser@users.noreply.github.com>
2022-10-08 23:13:05 +0200

    Update memory_helper.cpp

--- steam_overlay_hook/steam_overlay_hook/memory_helper.cpp
+++ steam_overlay_hook/steam_overlay_hook/memory_helper.cpp
@@ -2,27 +2,17 @@
 
 uintptr_t memory::PatternScan(const char* pattern, std::string mask, uintptr_t begin, uintptr_t size)
 {
-	size_t pattern_length = mask.length();
-	bool found = false;
-	uintptr_t pattern_pos = 0, return_address = 0;
+    size_t pattern_length = mask.length();
+    uintptr_t pattern_pos = 0;
 
-	for (uintptr_t i = 0; i < size - pattern_length; ++i) 
-	{
-		while (pattern_pos < pattern_length)
-		{
-			if (*(char*)(begin + i + pattern_pos) == pattern[pattern_pos] || mask[pattern_pos] == '?') {
-				if (pattern_pos == pattern_length - 1) {
-					return_address = begin + i;
-					found = true;
-				}
-			}
-			else {
-				pattern_pos = 0;
-				break;
-			}
-			pattern_pos++;
-		}
-		if (found == true) break;
-	}
-	return return_address;
+    for (uintptr_t i = 0; i < size - pattern_length; ++i) 
+    {
+        while (pattern_pos < pattern_length)
+        {
+            if (*(char*)(begin + i + pattern_pos) == pattern[pattern_pos] || mask[pattern_pos] == '?') { if (pattern_pos == pattern_length - 1) return begin + i; }
+            else { pattern_pos = 0; break; }
+            pattern_pos++;
+        }
+    }
+    return NULL;
 }
